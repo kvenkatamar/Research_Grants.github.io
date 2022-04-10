5e662b3ce6805973bbcec48a8fca300284d33da3
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2022-04-10 12:03:40 +0530

    JAVASCRIPT code
    Javascript code to implement slideshow feature.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let slideIndex = 1;
+// ----------------------------Script to Implement SLIDESHOW or CAROUSEL------------------------------
+
+var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -12,11 +14,15 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  else if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
