1dd8732011dc59f6837fbd6140102dcd7351abd3
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2022-04-24 06:04:56 +0530

    JAVASCRIPT code

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
-// ----------------------------Script to Implement SLIDESHOW or CAROUSEL------------------------------
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  else if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
 // ----------------------------Script to Implement SLICK SLIDER------------------------------
+// MAIN SLICK SLIDER
 
-$('.slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1500,
+$(document).ready(function () {
+  $(".main-slider").slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 1,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+// SLICK SLIDERs
+
+$(document).ready(function () {
+    $(".slider").slick({
+      arrows: true,
+      dots: false,
+      slidesToShow: 3,
+      autoplay: false,
+      speed: 500,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 550,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
 });
