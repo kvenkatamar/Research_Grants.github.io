13eff505a418266b738b0b9367b2651a37d62ddd
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2022-04-26 00:30:00 +0530

    JAVASCRIPT code

--- Subject-wise/app.js
+++ Subject-wise/app.js
@@ -1,90 +1,150 @@
 function displayInfo1(){
-    var x = document.querySelector("#info1");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info1");
+    var check = document.querySelector(".down-1");
+    var uncheck = document.querySelector(".up-1");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo2(){
-    var x = document.querySelector("#info2");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info2");
+    var check = document.querySelector(".down-2");
+    var uncheck = document.querySelector(".up-2");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo3(){
-    var x = document.querySelector("#info3");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info3");
+    var check = document.querySelector(".down-3");
+    var uncheck = document.querySelector(".up-3");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo4(){
-    var x = document.querySelector("#info4");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info4");
+    var check = document.querySelector(".down-4");
+    var uncheck = document.querySelector(".up-4");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo5(){
-    var x = document.querySelector("#info5");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info5");
+    var check = document.querySelector(".down-5");
+    var uncheck = document.querySelector(".up-5");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo6(){
-    var x = document.querySelector("#info6");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info6");
+    var check = document.querySelector(".down-6");
+    var uncheck = document.querySelector(".up-6");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo7(){
-    var x = document.querySelector("#info7");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info7");
+    var check = document.querySelector(".down-7");
+    var uncheck = document.querySelector(".up-7");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo8(){
-    var x = document.querySelector("#info8");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info8");
+    var check = document.querySelector(".down-8");
+    var uncheck = document.querySelector(".up-8");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo9(){
-    var x = document.querySelector("#info9");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info9");
+    var check = document.querySelector(".down-9");
+    var uncheck = document.querySelector(".up-9");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
 function displayInfo10(){
-    var x = document.querySelector("#info10");
-    if(x.style.display === "none" || x.style.display === ""){
-        x.style.display = "block";
+    var info = document.querySelector("#info10");
+    var check = document.querySelector(".down-10");
+    var uncheck = document.querySelector(".up-10");
+    if(info.style.display === "none" || info.style.display === ""){
+        info.style.display = "block";
+        check.style.display = "none";
+        uncheck.style.display = "block";
     }
     else{
-        x.style.display = "none";
+        info.style.display = "none";
+        check.style.display = "block";
+        uncheck.style.display = "none";
     }
 }
