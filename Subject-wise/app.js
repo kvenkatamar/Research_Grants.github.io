529738d52990f05bc90bd32813c436662a7fae16
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2022-04-25 23:33:57 +0530

    JAVASCRIPT code

---
+++ Subject-wise/app.js
@@ -0,0 +1,90 @@
+function displayInfo1(){
+    var x = document.querySelector("#info1");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo2(){
+    var x = document.querySelector("#info2");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo3(){
+    var x = document.querySelector("#info3");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo4(){
+    var x = document.querySelector("#info4");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo5(){
+    var x = document.querySelector("#info5");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo6(){
+    var x = document.querySelector("#info6");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo7(){
+    var x = document.querySelector("#info7");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo8(){
+    var x = document.querySelector("#info8");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo9(){
+    var x = document.querySelector("#info9");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+function displayInfo10(){
+    var x = document.querySelector("#info10");
+    if(x.style.display === "none" || x.style.display === ""){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
