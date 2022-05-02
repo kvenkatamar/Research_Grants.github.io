d461af63172dc9ced067bf7798d45c29f58257f3
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2022-05-02 21:40:11 +0530

    JAVASCRIPT code

--- Subject-wise/app.js
+++ Subject-wise/app.js
@@ -1,8 +1,52 @@
+function autoHide(){
+    document.querySelector("#info1").style.display = "none";
+    document.querySelector(".down-1").style.display = "block";
+    document.querySelector(".up-1").style.display = "none";
+
+    document.querySelector("#info2").style.display = "none";
+    document.querySelector(".down-2").style.display = "block";
+    document.querySelector(".up-2").style.display = "none";
+
+    document.querySelector("#info3").style.display = "none";
+    document.querySelector(".down-3").style.display = "block";
+    document.querySelector(".up-3").style.display = "none";
+
+    document.querySelector("#info4").style.display = "none";
+    document.querySelector(".down-4").style.display = "block";
+    document.querySelector(".up-4").style.display = "none";
+
+    document.querySelector("#info5").style.display = "none";
+    document.querySelector(".down-5").style.display = "block";
+    document.querySelector(".up-5").style.display = "none";
+
+    document.querySelector("#info6").style.display = "none";
+    document.querySelector(".down-6").style.display = "block";
+    document.querySelector(".up-6").style.display = "none";
+
+    document.querySelector("#info7").style.display = "none";
+    document.querySelector(".down-7").style.display = "block";
+    document.querySelector(".up-7").style.display = "none";
+
+    document.querySelector("#info8").style.display = "none";
+    document.querySelector(".down-8").style.display = "block";
+    document.querySelector(".up-8").style.display = "none";
+
+    document.querySelector("#info9").style.display = "none";
+    document.querySelector(".down-9").style.display = "block";
+    document.querySelector(".up-9").style.display = "none";
+
+    document.querySelector("#info10").style.display = "none";
+    document.querySelector(".down-10").style.display = "block";
+    document.querySelector(".up-10").style.display = "none";
+
+}
+
 function displayInfo1(){
     var info = document.querySelector("#info1");
     var check = document.querySelector(".down-1");
     var uncheck = document.querySelector(".up-1");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -18,6 +62,7 @@ function displayInfo2(){
     var check = document.querySelector(".down-2");
     var uncheck = document.querySelector(".up-2");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -33,6 +78,7 @@ function displayInfo3(){
     var check = document.querySelector(".down-3");
     var uncheck = document.querySelector(".up-3");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -48,6 +94,7 @@ function displayInfo4(){
     var check = document.querySelector(".down-4");
     var uncheck = document.querySelector(".up-4");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -63,6 +110,7 @@ function displayInfo5(){
     var check = document.querySelector(".down-5");
     var uncheck = document.querySelector(".up-5");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -78,6 +126,7 @@ function displayInfo6(){
     var check = document.querySelector(".down-6");
     var uncheck = document.querySelector(".up-6");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -93,6 +142,7 @@ function displayInfo7(){
     var check = document.querySelector(".down-7");
     var uncheck = document.querySelector(".up-7");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -108,6 +158,7 @@ function displayInfo8(){
     var check = document.querySelector(".down-8");
     var uncheck = document.querySelector(".up-8");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -123,6 +174,7 @@ function displayInfo9(){
     var check = document.querySelector(".down-9");
     var uncheck = document.querySelector(".up-9");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
@@ -138,6 +190,7 @@ function displayInfo10(){
     var check = document.querySelector(".down-10");
     var uncheck = document.querySelector(".up-10");
     if(info.style.display === "none" || info.style.display === ""){
+        autoHide();
         info.style.display = "block";
         check.style.display = "none";
         uncheck.style.display = "block";
